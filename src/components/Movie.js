8a5af08ebfa4c5444c53081ad02dd04d46dd7161
clianor <ejrgus94160@gmail.com>
2019-05-10 20:55:53 +0900

    Css 적용
    - nomad coders (movie-app)의 css참고

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import './Movie.css';
 import MoviePoster from './MoviePoster';
+import LinesEllipsis from 'react-lines-ellipsis'
 
 function Movie({ title, poster, genres, summary }) {
     return (
@@ -12,10 +14,20 @@ function Movie({ title, poster, genres, summary }) {
             </div>
             <div className="Movie__Column">
                 <h1>{title}</h1>
-                {genres.map((genre, index) => (
-                    <span key={index}>{genre}</span>
-                ))}
-                <p>{summary}</p>
+                <div>
+                    {genres.map((genre, index) => (
+                        <span key={index}>{genre}</span>
+                    ))}
+                </div>
+                <p>
+                    <LinesEllipsis 
+                        text={summary}
+                        maxLine='5'
+                        ellipsis=' ...'
+                        trimRight
+                        basedOn='letters'
+                    />
+                </p>
             </div>
         </div>
     );
